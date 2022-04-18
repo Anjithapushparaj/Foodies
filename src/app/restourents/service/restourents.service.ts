74206c056ba59e8f47b2619a5ed0dbc5edaf7842
Anjithapushparaj <anjitha.pushparaj@gmail.com>
2022-04-18 12:23:05 +0530

    cards added

--- src/app/restourents/service/restourents.service.ts
+++ src/app/restourents/service/restourents.service.ts
@@ -41,6 +41,42 @@ export class RestourentsService {
       name:'Burger King',
       foodItems:'Burger, Fast Food, Beverages',
 
+    },
+    {
+      image:'https://b.zmtcdn.com/data/pictures/chains/2/19663422/cb0815b8b04ef8613e7f00e43041c703.jpeg?fit=around|771.75:416.25&crop=771.75:416.25;*,*',
+      name:'The Aurum Cloud',
+      foodItems:'Kerala',
+
+    },
+    {
+      image:'https://b.zmtcdn.com/data/pictures/9/19196489/db449a30d51788c6d96afb13a56e1ed1.jpg',
+      name:'Charcoal Shack Restaurant',
+      foodItems:'Biryani, Arabian, Jamaican, Beverages',
+
+    },
+    {
+      image:'https://b.zmtcdn.com/data/pictures/7/901777/ffc63ad090e9535087762168fd68acd0.jpg',
+      name:'Soofi Mandhi',
+      foodItems:'Arabian, South Indian, Chettinad, Beverages',
+
+    },
+    {
+      image:'https://b.zmtcdn.com/data/pictures/8/19246688/782df4b5f24c8ce0c08242284f5dba45.jpg',
+      name:'Shawarma Daddy',
+      foodItems:'Lebanese, Arabian, Fast Food, Beverages',
+
+    },
+    {
+      image:'https://b.zmtcdn.com/data/pictures/7/19468387/80cd632d6ea31fcd65ee09d7eaa1ba2e.jpg',
+      name:'Foodx Restaurant',
+      foodItems:'Chinese, North Indian, South Indian, Beverages',
+
+    },
+    {
+      image:'https://b.zmtcdn.com/data/pictures/chains/7/902987/82fd142b05ebe39092b77133c6c5823a.jpg',
+      name:'The Ice Cream Factory',
+      foodItems:'Desserts, Ice Cream, Beverages, Shake',
+
     }
      
   ];
